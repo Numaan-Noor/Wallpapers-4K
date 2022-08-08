f087279295f4fc05989f8412dc3f4c7ebc32fc8f
Numaan-Noor <numaannoor81@gmail.com>
2022-08-08 15:43:57 +0530

    Add project files.

---
+++ Wallpapers-4K/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wallpapers_4K.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
