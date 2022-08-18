dd074f23718d29016b707afc0950e56060073257
Numaan-Noor <numaannoor81@gmail.com>
2022-08-18 15:23:27 +0530

    category

---
+++ Wallpapers-4K/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wallpapers_4K.Models;
+
+namespace Wallpapers_4K.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly WallpaperDbContext _context;
+
+        public CategoryController(WallpaperDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.categories.ToListAsync());
+        }
+
+
+
+        // GET: Category/Create
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Categories());
+            else
+                return View(_context.categories.Find(id));
+
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("Id,Name")] Categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                    _context.Add(category);
+                else
+                    _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+
+        // GET: Category/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var category = await _context.categories.FindAsync(id);
+            _context.categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
+
